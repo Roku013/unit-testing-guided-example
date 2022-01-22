c589798e48474c92a2ef3824598f45920f3cee70
Roku013 <roku.mail@gmail.com>
2022-01-22 12:52:56 +0100

    done

--- calculator.js
+++ calculator.js
@@ -1,17 +1,41 @@
 function sum(a, b) {
-  return;
+  if (!a && !b) {
+    return 0;
+  } else if (!a) {
+    return 0 + b;
+  } else if (!b) {
+    return a + 0;
+  } else {
+    return a + b;
+  }
 }
 
 function subtract(a, b) {
-  return;
+  if (!a && !b) {
+    return 0;
+  } else if (!a) {
+    return 0 - b;
+  } else if (!b) {
+    return a - 0;
+  } else {
+    return a - b;
+  }
 }
 
 function divide(a, b) {
-  return;
+  if (a === 0 || b === 0) {
+    return error;
+  } else {
+    return a / b;
+  }
 }
 
 function multiply(a, b) {
-  return a * b;
+  if (a === 0 || b === 0) {
+    return error;
+  } else {
+    return a * b;
+  }
 }
 
 // The following is required to make unit tests work. Please ignore it.
